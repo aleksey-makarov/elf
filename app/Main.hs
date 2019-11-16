6de92497163e4dc8a9d950833b13ef8d69326b9f
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-11-16 23:05:17 +0300

    hobjdump: wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,64 +1,96 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.ByteString as B
+import Data.ByteString.Char8 as BC8
+import Data.Char
 import Data.Elf
-import Data.Text.Prettyprint.Doc
+import Data.Text.Prettyprint.Doc as D
 import Data.Text.Prettyprint.Doc.Util
+import Data.List as L
+import Data.Word
+import Numeric
 import System.Environment
+
+formatBytestringChar :: ByteString -> Doc ()
+formatBytestringChar = hcat . L.map formatChar . BC8.unpack
+
+formatBytestringHex :: ByteString -> Doc ()
+formatBytestringHex = hsep . L.map formatHex . B.unpack
+
+formatBytestringLine :: ByteString -> Doc ()
+formatBytestringLine s = formatBytestringHex s <+> formatBytestringChar s
+
+splitBy :: Int -> ByteString -> [ByteString]
+splitBy n = L.unfoldr f
+    where
+        f s | B.null s = Nothing
+        f s | otherwise = Just $ B.splitAt n s
+
+formatChar :: Char -> Doc ()
+formatChar c = pretty $ if isAscii c && not (isControl c) then c else '.'
+
+formatHex :: Word8 -> Doc ()
+formatHex w = pretty $ case showHex w "" of
+    [ d ] -> [ '0', d ]
+    ww -> ww
+
+formatBytestring :: ByteString -> Doc ()
+formatBytestring = align . vsep . L.map formatBytestringLine . splitBy 16
+
+obj :: [Doc a] -> Doc a
+obj = D.group . encloseSep (flatAlt "{ " "{")
+                           (flatAlt " }" "}")
+                           ", "
+
+formatPairs :: [(Doc a, Doc a)] -> Doc a
+formatPairs ls = align $ obj $ fmap f ls
+    where
+        f (n, v) = fillBreak 20 (dquotes n <> ":") <+> v
 
 formatSection :: ElfSection -> Doc ()
 formatSection s =
-    let sEntries = [ ("elfSectionName:",      viaShow $elfSectionName s)
-                   , ("elfSectionType:",      viaShow $elfSectionType s)
-                   , ("elfSectionFlags:",     viaShow $elfSectionFlags s)
-                   , ("elfSectionAddr:",      viaShow $elfSectionAddr s)
-                   , ("elfSectionSize:",      viaShow $elfSectionSize s)
-                   , ("elfSectionLink:",      viaShow $elfSectionLink s)
-                   , ("elfSectionInfo:",      viaShow $elfSectionInfo s)
-                   , ("elfSectionAddrAlign:", viaShow $elfSectionAddrAlign s)
-                   , ("elfSectionEntSize:",   viaShow $elfSectionEntSize s)
-                   , ("elfSectionData:",      viaShow $elfSectionData s)
-                   ]
-        f (n, v) = fillBreak 20 n <+> v
-    in
-        align $ vsep (f <$> sEntries)
+    formatPairs [ ("elfSectionName",      viaShow $ elfSectionName s)
+                , ("elfSectionType",      viaShow $ elfSectionType s)
+                , ("elfSectionFlags",     viaShow $ elfSectionFlags s)
+                , ("elfSectionAddr",      viaShow $ elfSectionAddr s)
+                , ("elfSectionSize",      viaShow $ elfSectionSize s)
+                , ("elfSectionLink",      viaShow $ elfSectionLink s)
+                , ("elfSectionInfo",      viaShow $ elfSectionInfo s)
+                , ("elfSectionAddrAlign", viaShow $ elfSectionAddrAlign s)
+                , ("elfSectionEntSize",   viaShow $ elfSectionEntSize s)
+                , ("elfSectionData",      formatBytestring $ elfSectionData s)
+                ]
 
 formatSections :: [ElfSection] -> Doc ()
-formatSections s = braces $ sep $ punctuate comma $ formatSection <$> s
+formatSections s = align $ list $ formatSection <$> s
 
 formatSegment :: ElfSegment -> Doc ()
 formatSegment s =
-    let sEntries = [ ("elfSegmentType:",     viaShow $ elfSegmentType s)
-                   , ("elfSegmentFlags:",    viaShow $ elfSegmentFlags s)
-                   , ("elfSegmentVirtAddr:", viaShow $ elfSegmentVirtAddr s)
-                   , ("elfSegmentPhysAddr:", viaShow $ elfSegmentPhysAddr s)
-                   , ("elfSegmentAlign:",    viaShow $ elfSegmentAlign s)
-                   , ("elfSegmentData:",     viaShow $ elfSegmentData s)
-                   , ("elfSegmentMemSize:",  viaShow $ elfSegmentMemSize s)
-                   ]
-        f (n, v) = fillBreak 20 n <+> v
-    in
-        align $ vsep (f <$> sEntries)
+    formatPairs [ ("elfSegmentType",     viaShow $ elfSegmentType s)
+                , ("elfSegmentFlags",    viaShow $ elfSegmentFlags s)
+                , ("elfSegmentVirtAddr", viaShow $ elfSegmentVirtAddr s)
+                , ("elfSegmentPhysAddr", viaShow $ elfSegmentPhysAddr s)
+                , ("elfSegmentAlign",    viaShow $ elfSegmentAlign s)
+                , ("elfSegmentData",     formatBytestring $ elfSegmentData s)
+                , ("elfSegmentMemSize",  viaShow $ elfSegmentMemSize s)
+                ]
 
 formatSegments :: [ElfSegment] -> Doc ()
-formatSegments s = braces $ sep $ punctuate comma $ formatSegment <$> s
+formatSegments s = align $ list $ formatSegment <$> s
 
 formatElf :: Elf -> Doc ()
 formatElf elf =
-    let topEntries = [ ("elfClass:",      viaShow $ elfClass elf)
-                     , ("elfData:",       viaShow $ elfData elf)
-                     , ("elfVersion:",    viaShow $ elfVersion elf)
-                     , ("elfOSABI:",      viaShow $ elfOSABI elf)
-                     , ("elfABIVersion:", viaShow $ elfABIVersion elf)
-                     , ("elfType:",       viaShow $ elfType elf)
-                     , ("elfMachine:",    viaShow $ elfMachine elf)
-                     , ("elfEntry:",      viaShow $ elfEntry elf)
-                     , ("elfSections:",   formatSections $ elfSections elf)
-                     , ("elfSegments:",   formatSegments $ elfSegments elf)
-                     ]
-        f (n, v) = fillBreak 15 n <+> v
-    in
-        vsep (f <$> topEntries)
+    formatPairs [ ("elfClass",      viaShow $ elfClass elf)
+                , ("elfData",       viaShow $ elfData elf)
+                , ("elfVersion",    viaShow $ elfVersion elf)
+                , ("elfOSABI",      viaShow $ elfOSABI elf)
+                , ("elfABIVersion", viaShow $ elfABIVersion elf)
+                , ("elfType",       viaShow $ elfType elf)
+                , ("elfMachine",    viaShow $ elfMachine elf)
+                , ("elfEntry",      viaShow $ elfEntry elf)
+                , ("elfSections",   formatSections $ elfSections elf)
+                , ("elfSegments",   formatSegments $ elfSegments elf)
+                ]
 
 printElf :: String -> IO ()
 printElf fileName = do
