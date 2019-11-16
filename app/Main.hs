dd414cc671c0eaf829e034a3708e3b9cffc311c4
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-11-16 13:40:17 +0300

    hobjdump: wip

--- app/Main.hs
+++ app/Main.hs
@@ -6,19 +6,59 @@ import Data.Text.Prettyprint.Doc
 import Data.Text.Prettyprint.Doc.Util
 import System.Environment
 
+formatSection :: ElfSection -> Doc ()
+formatSection s =
+    let sEntries = [ ("elfSectionName:",      viaShow $elfSectionName s)
+                   , ("elfSectionType:",      viaShow $elfSectionType s)
+                   , ("elfSectionFlags:",     viaShow $elfSectionFlags s)
+                   , ("elfSectionAddr:",      viaShow $elfSectionAddr s)
+                   , ("elfSectionSize:",      viaShow $elfSectionSize s)
+                   , ("elfSectionLink:",      viaShow $elfSectionLink s)
+                   , ("elfSectionInfo:",      viaShow $elfSectionInfo s)
+                   , ("elfSectionAddrAlign:", viaShow $elfSectionAddrAlign s)
+                   , ("elfSectionEntSize:",   viaShow $elfSectionEntSize s)
+                   , ("elfSectionData:",      viaShow $elfSectionData s)
+                   ]
+        f (n, v) = fillBreak 20 n <+> v
+    in
+        align $ vsep (f <$> sEntries)
+
+formatSections :: [ElfSection] -> Doc ()
+formatSections s = braces $ sep $ punctuate comma $ formatSection <$> s
+
+formatSegment :: ElfSegment -> Doc ()
+formatSegment s =
+    let sEntries = [ ("elfSegmentType:",     viaShow $ elfSegmentType s)
+                   , ("elfSegmentFlags:",    viaShow $ elfSegmentFlags s)
+                   , ("elfSegmentVirtAddr:", viaShow $ elfSegmentVirtAddr s)
+                   , ("elfSegmentPhysAddr:", viaShow $ elfSegmentPhysAddr s)
+                   , ("elfSegmentAlign:",    viaShow $ elfSegmentAlign s)
+                   , ("elfSegmentData:",     viaShow $ elfSegmentData s)
+                   , ("elfSegmentMemSize:",  viaShow $ elfSegmentMemSize s)
+                   ]
+        f (n, v) = fillBreak 20 n <+> v
+    in
+        align $ vsep (f <$> sEntries)
+
+formatSegments :: [ElfSegment] -> Doc ()
+formatSegments s = braces $ sep $ punctuate comma $ formatSegment <$> s
+
 formatElf :: Elf -> Doc ()
 formatElf elf =
-    vsep [ "elfClass:" <+> (viaShow $ elfClass elf) -- ElfClass     Identifies the class of the object file.
-         , "elfData:"  <+> (viaShow $ elfData elf) -- ElfData      Identifies the data encoding of the object file.
-         , "elfVersion:" <+> (viaShow $ elfVersion elf) -- Int          Identifies the version of the object file format.
-         , "elfOSABI:" <+> (viaShow $ elfOSABI elf) -- ElfOSABI     Identifies the operating system and ABI for which the object is prepared.
-         , "elfABIVersion:" <+> (viaShow $ elfABIVersion elf) -- Int          Identifies the ABI version for which the object is prepared.
-         , "elfType:" <+> (viaShow $ elfType elf) -- ElfType      Identifies the object file type.
-         , "elfMachine:" <+> (viaShow $ elfMachine elf) -- ElfMachine   Identifies the target architecture.
-         , "elfEntry:" <+> (viaShow $ elfEntry elf) -- Word64       Virtual address of the program entry point. 0 for non-executable Elfs.
---         , elfSections   elf -- [ElfSection] List of sections in the file.
---         , elfSegments   elf -- [ElfSegment] List of segments in the file.
-         ]
+    let topEntries = [ ("elfClass:",      viaShow $ elfClass elf)
+                     , ("elfData:",       viaShow $ elfData elf)
+                     , ("elfVersion:",    viaShow $ elfVersion elf)
+                     , ("elfOSABI:",      viaShow $ elfOSABI elf)
+                     , ("elfABIVersion:", viaShow $ elfABIVersion elf)
+                     , ("elfType:",       viaShow $ elfType elf)
+                     , ("elfMachine:",    viaShow $ elfMachine elf)
+                     , ("elfEntry:",      viaShow $ elfEntry elf)
+                     , ("elfSections:",   formatSections $ elfSections elf)
+                     , ("elfSegments:",   formatSegments $ elfSegments elf)
+                     ]
+        f (n, v) = fillBreak 15 n <+> v
+    in
+        vsep (f <$> topEntries)
 
 printElf :: String -> IO ()
 printElf fileName = do
