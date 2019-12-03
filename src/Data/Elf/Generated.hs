5656651522cc04f3b26043e52f8604edcc66c1d3
Aleksey Makarov <aleksey.makarov@gmail.com>
2019-12-03 10:37:34 +0300

    implement Binary (Xe WordYY)

--- src/Data/Elf/Generated.hs
+++ src/Data/Elf/Generated.hs
@@ -13,6 +13,30 @@ import Data.Elf.TH
 newtype Be a = Be { fromBe :: a }
 newtype Le a = Le { fromLe :: a }
 
+instance Binary (Be Word16) where
+    put = putWord16be . fromBe
+    get = Be <$> getWord16be
+
+instance Binary (Le Word16) where
+    put = putWord16le . fromLe
+    get = Le <$> getWord16le
+
+instance Binary (Be Word32) where
+    put = putWord32be . fromBe
+    get = Be <$> getWord32be
+
+instance Binary (Le Word32) where
+    put = putWord32le . fromLe
+    get = Le <$> getWord32le
+
+instance Binary (Be Word64) where
+    put = putWord64be . fromBe
+    get = Be <$> getWord64be
+
+instance Binary (Le Word64) where
+    put = putWord64le . fromLe
+    get = Le <$> getWord64le
+
 $(mkDeclarations BaseWord8 "ElfOSABI" "ELFOSABI" "ELFOSABI_EXT"
     [ ("_SYSV",         0) -- No extensions or unspecified
     , ("_HPUX",         1) -- Hewlett-Packard HP-UX
