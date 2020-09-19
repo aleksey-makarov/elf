df9b01dd5a1208edaeba77e614c19d4e03c68e3f
Aleksey Makarov <aleksey.makarov@gmail.com>
2020-09-19 09:37:56 +0300

    wip

--- src/Data/Elf.hs
+++ src/Data/Elf.hs
@@ -37,6 +37,7 @@ import Data.Singletons
 import Data.Singletons.Sigma
 import Data.Word
 import Numeric.Interval as I
+import Numeric.Interval.NonEmpty as INE
 
 data ElfPart (c :: ElfClass)
     = ElfHeader
@@ -51,35 +52,62 @@ data ElfPart (c :: ElfClass)
         , epData   :: [ElfPart c]
         }
     | ElfSectionTable
-        { estInterval :: Interval Word64
-        }
+--        { estInterval :: Interval Word64
+--        }
     | ElfSegmentTable
-        { eptInterval :: Interval Word64
-        }
+--        { eptInterval :: Interval Word64
+--        }
 
-elfPartInterval :: forall a . SingI a => ElfPart a -> Interval Word64
-elfPartInterval ElfHeader{..} = 0 ... (fromIntegral $ headerSize $ fromSing $ sing @a) - 1
-elfPartInterval ElfSection{esHeader = SectionXX{..}} = if (sType == SHT_NOBITS) || (s == 0) then I.empty else o ... o + s - 1
+-- This can not be empty
+headerInterval :: forall a . SingI a => HeaderXX a -> INE.Interval Word64
+headerInterval _ = 0 INE.... (fromIntegral $ headerSize $ fromSing $ sing @a) - 1
+
+sectionTableInterval :: SingI a => HeaderXX a -> I.Interval Word64
+sectionTableInterval HeaderXX{..} = if (s == 0) then I.empty else o I.... o + s * n - 1
+    where
+        o = wxxToIntegral hShOff
+        s = fromIntegral  hShEntSize
+        n = fromIntegral  hShNum
+
+segmentTableInterval :: SingI a => HeaderXX a -> I.Interval Word64
+segmentTableInterval HeaderXX{..} = if (s == 0) then I.empty else o I.... o + s * n - 1
+    where
+        o = wxxToIntegral hPhOff
+        s = fromIntegral  hPhEntSize
+        n = fromIntegral  hPhNum
+
+sectionInterval :: SingI a => SectionXX a -> I.Interval Word64
+sectionInterval SectionXX{..} = if (sType == SHT_NOBITS) || (s == 0) then I.empty else o I.... o + s - 1
     where
         o = wxxToIntegral sOffset
         s = wxxToIntegral sSize
-elfPartInterval ElfSegment{epHeader = SegmentXX{..}} = if s == 0 then I.empty else o ... o + s - 1
+
+segmentInterval :: SingI a => SegmentXX a -> I.Interval Word64
+segmentInterval SegmentXX{..} = if s == 0 then I.empty else o I.... o + s - 1
     where
         o = wxxToIntegral pOffset
         s = wxxToIntegral pFileSize
-elfPartInterval ElfSectionTable{..} = estInterval
-elfPartInterval ElfSegmentTable{..} = eptInterval
+
+data LZip a = LZip [a] (Maybe a) [a]
+
+findInterval :: Ord t => t -> [(INE.Interval t, a)] -> LZip (INE.Interval t, a)
+findInterval e list = findInterval' [] list
+    where
+        findInterval' l []                              = LZip l Nothing []
+        findInterval' l (x : xs) | INE.member e (fst x) = LZip l (Just x) xs
+        findInterval' l (x : xs) | e < INE.inf (fst x)  = LZip l Nothing (x : xs)
+        findInterval' l (x : xs) | otherwise            = findInterval' xs (x : l)
 
 newtype Elf c = Elf [ElfPart c]
 
 parseElf' :: SingI a => HeaderXX a -> [SectionXX a] -> [SegmentXX a] -> BSL.ByteString -> Either String (Sigma ElfClass (TyCon1 Elf))
 parseElf' hdr ss ps bs =
-    let
-        hi = elfPartInterval $ ElfHeader hdr
-        -- hi = elfPartInterval $ ElfHeader hdr
-        -- hi = elfPartInterval hdr
-        -- hi = f sing
-    in
+--    let
+--        hi = elfPartInterval $ ElfHeader hdr
+--        -- hi = elfPartInterval $ ElfHeader hdr
+--        -- hi = elfPartInterval hdr
+--        -- hi = f sing
+--    in
         undefined
 
 parseElf :: BSL.ByteString -> Either String (Sigma ElfClass (TyCon1 Elf))
