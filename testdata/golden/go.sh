604f9bd6ced08a237d8db6216ad0eb1cdea5c232
Aleksey Makarov <aleksey.makarov@gmail.com>
2020-09-30 20:35:27 +0300

    wip: move test files

---
+++ testdata/golden/go.sh
@@ -0,0 +1,2 @@
+find . -type f -name '*.elf.golden' -delete
+find . -type f -name '*.header.golden' -delete
--- testdata/tiny.asm
+++
@@ -1,8 +0,0 @@
-SECTION .text
-
-global _start
-
-_start:
-    mov ebx,0
-    mov eax,1
-    int 0x80
